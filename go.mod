a35599a1effd400f677e0e0ff84c1c32c48d848e
socylx <socylx@163.com>
2022-10-01 10:42:02 +0800

    overseer

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module signin-go
 
 go 1.19
+
+require github.com/jpillora/overseer v1.1.6
+
+require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
+	github.com/jpillora/s3 v1.1.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/jpillora/overseer"
+)
+
+// prog(state) runs in a child process
+func prog(state overseer.State) {
+	log.Printf("app (%s) listening...", state.ID)
+	http.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "app (%s) says hello\n", state.ID)
+	}))
+	http.Serve(state.Listener, nil)
+}
+
+// create another main() to run the overseer process
+// and then convert your old main() into a 'prog(state)'
+func main() {
+	overseer.Run(overseer.Config{
+		Program: prog,
+		Address: ":3000",
+	})
 }
