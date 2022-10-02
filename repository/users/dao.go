9873b331b8ac7c9a67c1f89f92f21837e1f83f22
socylx <socylx@163.com>
2022-10-02 18:04:23 +0800

    context

--- repository/users/dao.go
+++ repository/users/dao.go
@@ -5,7 +5,8 @@ import "signin-go/internal/core"
 func (u *users) List(ctx core.Context, userID uint32) (user []*Users, err error) {
 	user = []*Users{}
 
-	err = u.db.Table(
+	db := u.db.WithContext(ctx.RequestContext())
+	err = db.Table(
 		u.TableName(),
 	).Where(
 		"users.is_del = 0 AND users.id = ?", userID,
