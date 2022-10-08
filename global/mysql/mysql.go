4dae1601e7cca41768b54a4693b8a851d6041092
socylx <socylx@163.com>
2022-10-08 22:39:13 +0800

    sqlDB

--- global/mysql/mysql.go
+++ global/mysql/mysql.go
@@ -54,13 +54,13 @@ func Init() {
 	}
 
 	// 设置连接池 用于设置最大打开的连接数，默认值为0表示不限制.设置最大的连接数，可以避免并发太高导致连接mysql出现too many connections的错误。
-	sqlDB.SetMaxOpenConns(0)
+	sqlDB.SetMaxOpenConns(100)
 
 	// 设置最大连接数 用于设置闲置的连接数.设置闲置的连接数则当开启的一个连接使用完成后可以放在池里等候下一次使用。
-	sqlDB.SetMaxIdleConns(80)
+	sqlDB.SetMaxIdleConns(10)
 
 	// 设置最大连接超时
-	sqlDB.SetConnMaxLifetime(time.Minute * 60)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 
 	// 使用插件
 	DB.Use(&TracePlugin{})
